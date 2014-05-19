5f499635c874382f1fdff9126f3062b7f5de58c2
Sebastian Walter <swalter@techfak.uni-bielefeld.de>
2014-05-19 14:54:02 +0200

    fixed output

--- terminals.php
+++ terminals.php
@@ -22,7 +22,7 @@ header('Content-Type: text/html; charset=utf-8');
 
 	<?php
         
-        function getEntities($classes,$yago,$categories,$properties,$second_language, $boolean, $d_host, $d_user, $d_pasw, $d_database){
+        function getEntities($classes,$yago,$categories,$properties,$second_language, $boolean, $numberterminals, $d_host, $d_user, $d_pasw, $d_database){
             
             $mysqli = new mysqli($d_host, $d_user, $d_pasw, $d_database);
             if ($mysqli->connect_errno) {
@@ -34,7 +34,9 @@ header('Content-Type: text/html; charset=utf-8');
             $terminals_language1 = array();
             $terminals_language2 = array();
             
-            if (strpos($second_language,'none') != false){
+                #echo $second_language;
+                #echo "<br>";
+            if ($second_language =='none'){
                 $second_language = "en";
             }
             
@@ -66,21 +68,23 @@ header('Content-Type: text/html; charset=utf-8');
             
             $where_part = substr($where_part, 0, -4);
             
-            $query ="SELECT DISTINCT en,".$second_language." FROM `resourcelabel`".$from_part." WHERE ".$where_part." LIMIT 0, 10 ;";
-            echo "generated query";
+            $query ="SELECT DISTINCT en,".$second_language." FROM `resourcelabel`".$from_part." WHERE ".$where_part." LIMIT 0, {$numberterminals} ;";
+                #echo "generated query";
             
-            if (strpos($boolean,'AND') != false){
+            if ($boolean== 'OR'){
                 $query = str_replace(" AND "," OR ",$query);
-                echo "OR CASE";
+                #echo "OR CASE";
             }
             
-            #SELECT en FROM `resourcelabel`, (SELECT resourceid FROM `classrelation` WHERE classid = (SELECT id FROM `classes` WHERE uri='http://dbpedia.org/ontology/PopulatedPlace'))as test WHERE resourcelabel.id = test.resourceid;
+                #echo $query;
+                #echo "<br>";
             
             if ($result = $mysqli->query($query)) {
                 foreach($result as $r){
                     $test = array_values($r)[0];
                     array_push($terminals_language1, $test);
-                    
+                    #echo $test;
+                    #echo "<br>";
                     $test2 = array_values($r)[1];
                     array_push($terminals_language2, $test2);
                 }
@@ -92,8 +96,6 @@ header('Content-Type: text/html; charset=utf-8');
             mysqli_close($mysqli);
             }
             
-            echo "run query";
-            
             return array($terminals_language1,$terminals_language2);
         }
         
@@ -103,6 +105,8 @@ header('Content-Type: text/html; charset=utf-8');
         $d_user = "dbpedia";
         $d_pasw = "";
         $d_database = "dbpedia";
+        
+        
         
         
         if(isset($_POST['setTerminals'])|| isset($_POST['setTerminals2'])){
@@ -192,7 +196,7 @@ header('Content-Type: text/html; charset=utf-8');
     	$boolean = ($_POST['boolean']);
         
         
-        $entities = getEntities($classes,$yago,$categories,$properties,$second_language, $boolean, $d_host, $d_user, $d_pasw, $d_database);
+        $entities = getEntities($classes,$yago,$categories,$properties,$second_language, $boolean, $numberterminals, $d_host, $d_user, $d_pasw, $d_database);
 
     	
     	echo "<body> <div id=\"terminal-start\">";
@@ -201,7 +205,7 @@ header('Content-Type: text/html; charset=utf-8');
 
         
 		echo "<br>";
-		echo "Retrieved ".count($entities)." terminals";
+		#echo "Retrieved ".count(array_values($entities)[1])." terminals";
 		echo "<br>";
 		echo "<br>";
 		$abnf = "#ABNF 1.0 UTF-8; \n Terminals = ";
@@ -211,31 +215,31 @@ header('Content-Type: text/html; charset=utf-8');
         echo "<td><h2>en</h2></td>";
         echo "<td><h2>$second_language</h2></td>";
         echo "</tr>";
-		foreach($entities as $entry){
-            echo "<tr>";
+        $entities1 = $entities[0];
+        $entities2 = $entities[1];
+		while (($element1 = next($entities1)) !== false) {
+            $element2 = next($entities2);
 			$name= "";
 			$name2= "";
-			if (count(array_values($entry))==2){
-				$name = array_values($entry)[1];
-				#echo $name;
-                echo "<td><input type=\"checkbox\" name=\"setTerminals[]\" value=\"$name\" id=\"id{$name}\" \"/>";
-                echo "<label for=\"id{$name}\"> $name</label></td>";
-                $tmp = array_values($entry)[0];
-				if (count($tmp)>0 && $tmp!=""){
-					#echo " -- ".$tmp;
-					$name2 = $tmp;
-                    #$name = $name." -- ".$tmp;
-                    echo "<td><input type=\"checkbox\" name=\"setTerminals2[]\" value=\"$name2\" id=\"id{$name2}\" \"/>";
-                    echo "<label for=\"id{$name2}\"> $name2</label></td>";
-				}
-                
-                
-			}
-            echo "</tr>";
+            $name = $element1;
+            #echo $name;
+            echo "<td><input type=\"checkbox\" name=\"setTerminals[]\" value=\"$name\" id=\"id{$name}\" \"/>";
+            echo "<label for=\"id{$name}\"> $name</label></td>";
+            $name2 = $element2;
+            if($name2 == ""){
+                echo "<td></td>";
+            }
+            else{
+                echo "<td><input type=\"checkbox\" name=\"setTerminals2[]\" value=\"$name2\" id=\"id{$name2}\" \"/>";
+                echo "<label for=\"id{$name2}\"> $name2</label></td>";
+            }
+            
 
+            echo "</tr>";
+            
 			$abnf ="{$abnf} {$name} | ";
 			if (count($name2)>0){
-				$abnf2 ="{$abnf2} {$name2} | ";
+            		$abnf2 ="{$abnf2} {$name2} | ";
 			}
             #echo "<br>";
 				
